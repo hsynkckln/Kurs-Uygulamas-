50a5092234d7ec04f51e6cded2e785a62d2dc6bd
Huseyin <kucukalanhuseyin@gmail.com>
2022-08-15 00:35:57 +0300

    commit edildi

---
+++ appES6.js
@@ -0,0 +1,173 @@
+class Course{
+    constructor(title,instructor,image){
+        this.courseId=Math.floor(Math.random()*10000);
+        this.title=title;
+        this.instructor=instructor;
+        this.image=image;
+    }
+}
+
+class UI{
+    addCourseToList(course){
+        const list=document.getElementById('course-list');
+
+        var html=`
+               <tr>
+                  <td><img src="img/${course.image}"/></td>
+                  <td>${course.title}</td>
+                  <td>${course.instructor}</td>
+                  <td><a href="#" data-id="${course.courseId}" class="btn btn-danger btn-sm delete">Delete</a></td>
+               </tr>
+        `;
+        list.innerHTML+=html;
+
+    }
+    deleteCourse(elements){
+        if(elements.classList.contains('delete')){
+            elements.parentElement.parentElement.remove();
+
+            return true;
+        };
+
+    }
+    cleanControls(){
+        const title=document.getElementById('title').value="";
+        const instructor=document.getElementById('instructor').value="";
+        const image=document.getElementById('image').value="";
+
+    }
+    showAlert(message,className){
+        var alert=`
+        <div class="alert alert-${className}">
+             ${message}
+        </div>
+       
+       `;
+   
+       var row=document.querySelector('.row');
+       //beforeBegin,afterBegin,beforeEnd,afterEnd
+       row.insertAdjacentHTML('beforeBegin',alert);
+   
+       setTimeout(()=>{
+           document.querySelector('.alert').remove();
+   
+       },3000);
+
+    }
+}
+
+
+class Storage{
+
+    static getCourses(){
+        let courses;
+
+        if(localStorage.getItem('courses')===null){
+            courses=[];
+        }else{
+            courses=JSON.parse(localStorage.getItem('courses'));
+        }
+
+        return courses;
+
+    }
+    static displayCourses(){
+        const courses=Storage.getCourses();
+
+        courses.forEach(course => {
+            const ui=new UI();
+            ui.addCourseToList(course);
+            
+        });
+
+    }
+    static addCourse(course){
+        const courses=Storage.getCourses();
+        courses.push(course);
+        localStorage.setItem('courses',JSON.stringify(courses));
+
+    }
+    static deleteCourse(element){
+        if(element.classList.contains('delete')){
+            const id=element.getAttribute('data-id');
+            
+            const courses=Storage.getCourses();
+
+            courses.forEach((course,index)=>{
+                if(course.courseId==id){
+                    courses.splice(index,1);//indexten sonra 1 tane sil demek bu..
+                }
+            });
+
+            localStorage.setItem('courses',JSON.stringify(courses));
+        }
+
+
+    }
+}
+
+document.addEventListener('DOMContentLoaded',Storage.displayCourses);
+
+
+const form=document.getElementById('new-course');
+
+form.addEventListener('submit',function(e){
+
+    const title=document.getElementById('title').value;
+    const instructor=document.getElementById('instructor').value;
+    const image=document.getElementById('image').value;
+
+    //create course object
+    const course=new Course(title,instructor,image);
+
+    //create UI
+    const ui=new UI();
+
+    
+
+
+    if(title==='' || instructor==='' || image===''){
+        ui.showAlert('Lütfen formu tamamlayın','warning');
+    }
+    else{
+
+
+        //add course to list
+        ui.addCourseToList(course);
+
+        //save to LS
+        Storage.addCourse(course);
+
+        //clear controls
+        ui.cleanControls();
+
+        ui.showAlert('Kurs Eklendi','success');
+
+    }
+    
+
+
+    
+
+    //show on the ui
+
+    e.preventDefault();
+});
+
+document.getElementById('course-list').addEventListener('click',function(e){
+    const ui=new UI();
+
+    if(ui.deleteCourse(e.target)==true){
+
+         //delete from LS
+         Storage.deleteCourse(e.target);
+
+
+         ui.showAlert('Kurs Silindi','danger');
+
+    }
+    
+
+   
+
+});
